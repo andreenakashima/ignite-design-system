9ee0f378c52944d4f8196a16116482c737a0773c
andreenakashima <andreenakashima@gmail.com>
2022-10-24 21:36:17 -0300

    Controles nos componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/andreenakashima.png',
     alt: 'Andre Nakashima',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
